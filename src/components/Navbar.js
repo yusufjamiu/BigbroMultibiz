34ec5b9a57b46ee97c57a5a3a60fb7e5c04d75ed
yusufjamiu <princeyusufjamiuadegbolahan@gmail.com>
2024-11-30 18:26:08 +0100

    Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 const Navbar = () => {
   return (
     <nav className="flex justify-between bg-white-300 px-4 py-3 items-center mb-6">
-      <div className="text-xl font-bold">BigBro Multibiz</div>
+      <div className="text-xl font-bold text-blue-600">BigBroMultibiz</div>
       <div className="flex space-x-4">
         <a href="https://www.facebook.com/share/1BgkP2FREY/?mibextid=LQQJ4d" className="text-blue-600 hover:text-gray-700">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
-        <a href="https://twitter.com" className="text-blue-500 hover:text-gray-700">
+        <a href="https://twitter.com" className="text-blue-600 hover:text-gray-700">
           <FontAwesomeIcon icon={faTwitter} />
         </a>
         <a href="https://www.instagram.com/bigbromultibiz_ng/profilecard/?igsh=MWtrenhnbXI5eDlyYw==" className="text-blue-600 hover:text-gray-700">
